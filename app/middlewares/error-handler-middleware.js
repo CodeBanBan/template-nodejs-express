2867ef3609ad1145575eb86546fb0222f0f308ed
Tatthep <tatthep.c@hoteliers.guru>
2019-02-15 10:12:53 +0700

    fix error handler to express standard

--- app/middlewares/error-handler-middleware.js
+++ app/middlewares/error-handler-middleware.js
@@ -1,19 +1,9 @@
 'use strict'
 
-const NODE_ENV = process.env.NODE_ENV || 'dev'
-
 const HTTPStatus = require('http-status')
 const _ = require('lodash')
 
-function claudiaHandler (req, res, next, err) {
-  errorHandler(res, err)
-}
-
-function expressHandler (err, req, res, next) {
-  errorHandler(res, err)
-}
-
-function errorHandler (res, err) {
+function errorHandlerMiddleware (err, req, res, next) {
   let code = err.status || HTTPStatus.INTERNAL_SERVER_ERROR
   let message = (err && err.message) || err
   let type = (err && err.type)
@@ -23,4 +13,4 @@ function errorHandler (res, err) {
   res.json(result)
 }
 
-module.exports = (NODE_ENV === 'prod') ? claudiaHandler : expressHandler
+module.exports = errorHandlerMiddleware
