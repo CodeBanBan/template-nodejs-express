4746628552ae62be636db20089851f1945011b25
Tatthep <tatthep.c@hoteliers.guru>
2019-02-20 11:10:57 +0700

    fix(aws-config): fix error cannot find AWS credential

--- app/configs/dynamodb-config.js
+++ app/configs/dynamodb-config.js
@@ -1,31 +1,34 @@
 'use strict'
 
 const NODE_ENV = process.env.NODE_ENV || 'dev'
+const IS_CI = process.env.IS_CI || ''
 
 const AWS_ACCESS_KEY_ID = 'ACCESS KEY'
 const AWS_SECRET_ACCESS_KEY = 'SECRET KEY'
 const AWS_REGION = 'ap-northeast-1'
 
 const ENDPOINT_DEV = 'http://localhost:8000'
-const ENDPOINT_TEST = 'http://localhost:8100'
+const ENDPOINT_TEST_LOCAL = 'http://localhost:8100'
+const ENDPOINT_TEST_CI = 'http://dynamodb:8100'
+const ENDPOINT_TEST = (IS_CI === 'ci') ? ENDPOINT_TEST_CI : ENDPOINT_TEST_LOCAL
 const ENDPOINT_URL = (NODE_ENV === 'test') ? ENDPOINT_TEST : ENDPOINT_DEV
 
 const TABLE_CONFIG = {
   PREFIX: 'sample_'
 }
 
-const AWS_CONFIG = {
+const AWS_CONFIG_DEV = {
+  accessKeyId: AWS_ACCESS_KEY_ID,
+  secretAccessKey: AWS_SECRET_ACCESS_KEY,
   region: AWS_REGION
 }
 
-// --- Remove this block if use AWS-Lambda --- //
-if (NODE_ENV === 'prod') {
-  AWS_CONFIG.accessKeyId = AWS_ACCESS_KEY_ID
-  AWS_CONFIG.secretAccessKey = AWS_SECRET_ACCESS_KEY
+const AWS_CONFIG_PROD = {
+  region: AWS_REGION
 }
 
 module.exports = {
-  AWS_CONFIG,
+  AWS_CONFIG: (NODE_ENV === 'prod') ? AWS_CONFIG_PROD : AWS_CONFIG_DEV,
   TABLE_CONFIG,
   ENDPOINT_URL
 }
