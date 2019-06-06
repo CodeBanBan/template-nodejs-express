3a11531d710e83bb90d9872f15703c984f417eba
Tatthep <tatthep.c@hoteliers.guru>
2019-06-06 11:39:55 +0700

    add Domains structure

--- app/controllers/hello-controller.js
+++ app/controllers/hello-controller.js
@@ -2,13 +2,16 @@
 
 const _ = require('lodash')
 const HTTPStatus = require('http-status')
+const HelloDomain = require('../domains/hello-domain')
 const logger = require('../helpers/logger')
 
 async function hello (req, res, next) {
   logger.info('Log Info: hello controller')
   const name = req.params.name || '[No Name]'
+  const message = HelloDomain.helloWithName(name)
+
   res.json({
-    message: `hello world ${name}`
+    message: message
   })
 }
 
---
+++ app/domains/hello-domain.js
@@ -0,0 +1,9 @@
+'use strict'
+
+function helloWithName (name = '[No Name]') {
+  return `hello world ${name}`
+}
+
+module.exports = {
+  helloWithName
+}
